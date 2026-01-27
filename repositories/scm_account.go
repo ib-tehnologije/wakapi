11391e383259a14289d5996b51fde0f5457121c3
Igor Benić <igbenic@icloud.com>
2026-01-27 13:50:38 +0100

    new tests

--- repositories/scm_account.go
+++ repositories/scm_account.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"github.com/muety/wakapi/models"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type ScmAccountRepository struct {
@@ -15,7 +16,10 @@ func NewScmAccountRepository(db *gorm.DB) *ScmAccountRepository {
 
 func (r *ScmAccountRepository) Upsert(acc *models.ScmAccount) error {
 	return r.db.
-		Clauses(clauseOnConflictDoUpdateAll()).
+		Clauses(clause.OnConflict{
+			Columns:   []clause.Column{{Name: "user_id"}, {Name: "provider"}},
+			DoUpdates: clause.AssignmentColumns([]string{"auth_type", "access_token_enc", "refresh_token_enc", "installation_id", "expires_at", "provider_user_id", "provider_login", "avatar_url", "updated_at", "revoked_at"}),
+		}).
 		Create(acc).Error
 }
 
